f796d5d546070c2afe3a2b7a3479a9e645ea790a
SamedOrd <samed.ordueris@gmail.com>
2022-12-04 21:08:09 +0300

    odev7

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
-# Mekanbul REST API TEST
-### 1. API Collections Test
-![2](resimler/APICollectionTest.png)
-
-### 2. API Test
-![1](/resimler/TestAPI.png)
+https://mekanbul.samedordueri.repl.co/?enlem=37&boylam=35
--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,95 +1,107 @@
-const anaSayfa=function(req,res,next){
-    res.render('anaSayfa', { 
-        "baslik": "Anasayfa",
-        "sayfaBaslik": {
-            "siteAd": "Mekanbul",
-            "slogan": "Civardaki Mekanları Keşfet!"
-        },
-        "mekanlar": [
-            {
-                "ad": "Starbucks",
-                "adres": "Fatih mah.",
-                "puan": "4",
-                "imkanlar": ["Kahveler", "Kekler", "Donuts"],
-                "mesafe": "4km"
-            }
-            ,
-            {
-                "ad": "D'OR Bistro & Lounge",
-                "adres": "Fatih mah.",
-                "puan": "4",
-                "imkanlar": ["Kahveler", "Kekler", "Pastalar"],
-                "mesafe": "3km"
-            }
-            ,
-            {
-                "ad": "Arabica Coffee House",
-                "adres": "Iyaş Park Avm",
-                "puan": "4",
-                "imkanlar": ["Kahveler", "Cookie", "Pastalar","Cheesecake"],
-                "mesafe": "1km"
-    
-            }
-            
+var express = require("express");
+var router = express.Router();
+const axios = require("axios");
 
-        ]
-
-     });
+var apiSecenekleri = {
+  sunucu: "http://localhost:3000",
+  apiYolu: "/api/mekanlar/",
+};
+var mesafeyiFormatla = function(mesafe){
+  var yeniMesafe, birim;
+  if(mesafe>1){
+    yeniMesafe = parseFloat(mesafe).toFixed(1);
+    birim = " km";
+  }
+  else{
+    yeniMesafe = parseInt(mesafe*1000,10);
+    birim = " m";
+  }
+  return yeniMesafe + birim;
 }
 
-const mekanBilgisi=function(req,res,next){
-    res.render('mekanBilgisi', { 
-        "baslik": "Mekan Bilgisi,",
-        "mekanBaslik": "Starbucks",
-        "mekanDetay":{
-            "ad": "Starbucks",
-            "adres": "Fatih mah.",
-            "puan":"4",
-            "imkanlar": ["Kahveler","Kekler","Donuts"],
-            "kordinatlar": {
-                "enlem": "37.7",
-                "boylam": "30.5",
-            },
-            "saater": [
-                {
-                    "gunler": "Pazartesi-Cuma",
-                    "acilis": "09.00",
-                    "kapanis": "23.00",
-                    "kapali": false
-                }
-                ,
-                {
-                    "gunler": "Cumartesi-Pazar",
-                    "acilis": "10.00",
-                    "kapanis": "22.00",
-                    "kapali": false
-                }
-            ],
-            "yorumlar": [ {
-                "yorumYapan": "Samed Ordueri",
-                "puan": "4",
-                "tarih": "24 Mayıs 2021",
-                "yorumMetni": "Kahveler fena değil ama yer bulmak bir hayli zor."
-
-            }
-              
-            ]
-        }
-
-
-        
-    });
+var anaSayfaOlustur = function(res,mekanListesi){
+  var mesaj;
+  if(!(mekanListesi instanceof Array)){
+    mesaj = "API HATASI: Bir şeyler ters gitti.";
+    mekanListesi=[];
+  }
+  else{
+    if(!mekanListesi.length){
+      mesaj="Civarda herhangi bir mekan yok.";
+    }
+  }
+  res.render("anasayfa",{
+    baslik:"Anasayfa",
+    sayfaBaslik:{
+    siteAd:"Mekanbul",
+    slogan:"Mekanları Keşfet",
+  },
+  mekanlar:mekanListesi,
+  mesaj:mesaj,
+  });
 }
+const anaSayfa = function (req, res) {
+  axios.get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu,{
+    params:{
+      enlem:req.query.enlem,
+      boylam:req.query.boylam,
+    },
+  }).then(function(response){
+    var i,mekanlar;
+    mekanlar = response.data;
+    for (i=0;i<mekanlar.length;i++){
+      mekanlar[i].mesafe=mesafeyiFormatla(mekanlar[i].mesafe);
+    }
+    anaSayfaOlustur(res,mekanlar);
+  }).catch(function(hata){
+    anaSayfaOlustur(res,hata);
+  });
+};
+var detaySayfasiOlustur = function(res,mekanDetaylari){
+  mekanDetaylari.koordinat={
+    "enlem": mekanDetaylari.koordinat[0],
+    "boylam": mekanDetaylari.koordinat[1],
+  }
+  res.render('mekanbilgisi',
+  {
+    mekanBaslik: mekanDetaylari.ad,
+    mekanDetay: mekanDetaylari
+  });
+};
+var hataGoster = function(res,hata){
+  var mesaj;
+  if(hata.response.status=404){
+    mesaj= "404, Sayfa bulunamadı!";
+  }
+  else{
+    mesaj = hata.response.status + " hatası";
+  }
+  res.status(hata.response.status)
+  res.render('error', {
+    "mesaj":mesaj
+  });
+};
 
-const yorumEkle=function(req,res,next){
-    res.render('yorumEkle', { title: 'yorumEkle' });
-}
-const hakkinda=function(req,res,next){
-    res.render('hakkinda', { title: 'Hakkinda' });
-}
-module.exports ={
-    anaSayfa,
-    mekanBilgisi,
-    yorumEkle,
-    hakkinda
-}
+const mekanBilgisi = function (req, res) {
+  axios
+  .get(apiSecenekleri.sunucu + apiSecenekleri.apiYolu + req.params.mekanid)
+  .then(function(response){
+    detaySayfasiOlustur(res,response.data);
+  })
+  .catch(function(hata){
+    hataGoster(res,hata);
+  })
+};
+
+
+const yorumEkle = function (req, res, next) {
+  res.render("yorumekle", { title: "Yorum Ekle" });
+};
+
+module.exports = {
+  anaSayfa,
+  mekanBilgisi,
+  yorumEkle,
+  anaSayfaOlustur,
+  mesafeyiFormatla
+};
