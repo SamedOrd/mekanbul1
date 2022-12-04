9cdd5cc89421fd319ea4445a100635b3be160cfb
SamedOrd <samed.ordueris@gmail.com>
2022-12-04 21:46:47 +0300

    odev7

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -4,17 +4,15 @@ var ctrlMekanlar = require('../controllers/mekanlar');
 var ctrlYorumlar = require('../controllers/yorumlar');
 
 router
-.route('/mekanlar')
-.get(ctrlMekanlar.mekanlariListele)
-.post(ctrlMekanlar.mekanEkle);
-
-router
 .route('/mekanlar/:mekanid')
 .get(ctrlMekanlar.mekanGetir)
 .put(ctrlMekanlar.mekanGuncelle)
 .delete(ctrlMekanlar.mekanSil);
 
-
+router
+.route('/mekanlar')
+.get(ctrlMekanlar.mekanlariListele)
+.post(ctrlMekanlar.mekanEkle);
 
 router
 .route("/mekanlar/:mekanid/yorumlar")
